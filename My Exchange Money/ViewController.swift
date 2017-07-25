4b34a78d7ed74a5f5f5108e86d90abc702043bbf
MasterUNG <phrombutr@gmail.com>
2017-07-25 16:58:24 +0700

    Success

--- My Exchange Money/ViewController.swift
+++ My Exchange Money/ViewController.swift
@@ -9,6 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var answerOutlet: UILabel!
+    
+    @IBOutlet weak var numberOutlet: UITextField!
+    
+    
+    @IBAction func exchangeButton(_ sender: Any) {
+        
+        let factor = 33.46
+        
+        let douMoney: Double = Double(numberOutlet.text!)!
+        let douAnswer = douMoney * factor
+        
+        answerOutlet.text = "เงินบาทที่แลกได้ \(douAnswer)"
+        
+        
+        
+        
+        
+    }
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
